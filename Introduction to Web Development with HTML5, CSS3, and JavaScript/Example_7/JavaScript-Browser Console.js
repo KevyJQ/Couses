f4a138b11dc0e1d168df8e28761028dc44042ab3
Kevy <kevy.bryan@gmail.com>
2023-12-15 12:20:25 -0600

    Ejemplos y problemas realizados en el curso

---
+++ Introduction to Web Development with HTML5, CSS3, and JavaScript/Example_7/JavaScript-Browser Console.js
@@ -0,0 +1,42 @@
+/*
+Para ejecutar la siguiente comenado, en mi caso estoy implementando la herramienta "node"
+
+Para correr el siguiente comando hacemos lo siquiente
+	1- Habrimos la terminal
+	2- Nos vamos a la direccion donde se encuantra nuesto archivo
+	3- corremos el siquiente comando $node archivo.js
+*/
+
+//Example 1
+/*
+console.log("Hello world!")	
+*/
+
+// ---------------------------------------------------------------------------------
+//Example 2
+/*
+let y var son palabras reservadas que se utiliazan para declarar variables. La principal diferencia entre las 2 es:
+	let
+		crea una variable de ambito local
+	var
+		crea una variable de ambito global
+
+*/
+/*
+let num = 5	//La variable solo es accesible dentro del ambito en el que se declara
+var mystr = "John"	//La variable es accesible desde cualquier lugar del script
+console.log(num)
+console.log(mystr)
+*/
+
+// ---------------------------------------------------------------------------------
+//Example 2
+
+// ---------------------------------------------------------------------------------
+//Example 2
+
+// ---------------------------------------------------------------------------------
+//Example 2
+
+// ---------------------------------------------------------------------------------
+//Example 2
